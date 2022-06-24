d3de950c5dd6dcf5e95e7819b0594a295be7e530
Sebastian Hoof <hoof@uni-goettingen.de>
2022-06-24 22:06:29 +0200

    Added QCD axion ex.

--- test-axion.py
+++ test-axion.py
@@ -6,11 +6,9 @@ import matplotlib.pyplot as plt
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(script_path+"/../../../simple_example/")
-from example_solutions import theta, dtheta_dtau
+from example_solutions import theta, dtheta_dtau, tau_osc, v_ma, v_aux, v_aux_f2
 
-def test_solve_alp():
-    thetai = 2.0 # Initial field value
-
+def test_solve_alp(thetai=2.0):
     w = lambda tau: 1.5*np.ones_like(tau)
     g = lambda tau: 0.75/tau
 
@@ -22,7 +20,7 @@ def test_solve_alp():
     yi = thetai
     dyi = 0
 
-    eps = 1e-6
+    eps = 1e-10
     epsh = 1e-12
     xs, ys, dys, ss, ps, types = riccati.solve(w, g, ti, tf, yi, dyi, eps = eps, epsh = epsh)
     xs = np.array(xs)
@@ -45,6 +43,32 @@ def test_solve_alp():
     ax[0].legend()
     plt.show()
 
+def test_solve_harmonic_qcd(thetai = 2.0):
+    w = lambda tau: v_ma(-tau_osc*tau)*v_aux(-tau)
+    g = lambda tau: -0.5*v_aux_f2(-tau_osc*tau)/tau
+
+    ti = -50.0
+    tf = -0.1
+    yi = thetai
+    dyi = 0
+
+    eps = 1e-6
+    epsh = 1e-12
+    xs, ys, dys, ss, ps, types = riccati.solve(w, g, ti, tf, yi, dyi, eps = eps, epsh = epsh)
+    xs = np.flip(np.log10(-np.array(xs)))
+    ys = np.flip(np.array(ys))
+    types = np.flip(np.array(types))
+    
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    ax[0].plot(xs[types==0], ys[types==0], '.-', color='C1', label='Nonosc step')
+    ax[0].plot(xs[types==1], ys[types==1], '.-', color='C0', label='Osc step')
+    ax[0].set_ylabel("$y(x)$")
+    ax[1].set_ylabel("Relative error")
+    ax[1].set_xlabel("$x$")
+    ax[0].legend()
+    plt.show()
+
 
 if __name__ == "__main__":
-    test_solve_alp()
+    #test_solve_alp()
+    test_solve_harmonic_qcd()
