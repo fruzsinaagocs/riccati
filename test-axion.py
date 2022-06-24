f68082c4aa744e6390422a1844b1be3bbbdf397e
Sebastian Hoof <hoof@uni-goettingen.de>
2022-06-24 21:26:26 +0200

    Fixed eq for simple ALP ex.

--- test-axion.py
+++ test-axion.py
@@ -11,18 +11,18 @@ from example_solutions import theta, dtheta_dtau
 def test_solve_alp():
     thetai = 2.0 # Initial field value
 
-    w = lambda tau: 2.25*np.ones_like(tau)
-    g = lambda tau: -0.5*1.5/tau
+    w = lambda tau: 1.5*np.ones_like(tau)
+    g = lambda tau: 0.75/tau
 
     alpth = lambda tau: theta(tau, thetai)
     alpdth = lambda tau: dtheta_dtau(tau, thetai)
 
-    ti = 1e-10
-    tf = 20.0
+    ti = 1e-5
+    tf = 50.0
     yi = thetai
     dyi = 0
 
-    eps = 1e-4
+    eps = 1e-6
     epsh = 1e-12
     xs, ys, dys, ss, ps, types = riccati.solve(w, g, ti, tf, yi, dyi, eps = eps, epsh = epsh)
     xs = np.array(xs)
@@ -30,7 +30,7 @@ def test_solve_alp():
     types = np.array(types)
     ytrue = alpth(xs)
     ncts = 5000
-    xcts = np.linspace(ti, tf, ncts)
+    xcts = np.linspace(ti, xs[-1], ncts)
     ycts = alpth(xcts)
     yerr = np.abs((ytrue - ys))/np.abs(ytrue)
     
