6e75f84294d5946d64284b79c11528724b5a225a
Fruzsina Agocs <agocs.fruzsina@gmail.com>
2023-01-24 21:51:20 -0500

    Small fix in evolve.py to avoid dense output being assumed between different runs of solve(), progress on docs

--- riccati/__init__.py
+++ riccati/__init__.py
@@ -5,4 +5,6 @@ Some docstring here!
 """
 
 from riccati.evolve import solve, osc_evolve, nonosc_evolve
+from riccati.step import nonosc_step, osc_step
+from riccati.stepsize import choose_nonosc_stepsize, choose_osc_stepsize
 from riccati.solversetup import solversetup
